4a99592fad0c337eb58a1973a888779f4b8e2c83
shankar sajith <shankarsajith.dev@gmail.com>
2025-10-28 13:33:53 +0530

    implimented spectral modularity partitioning

--- Kclub.py
+++ Kclub.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -12,7 +13,10 @@ k = np.array([np.sum(A,axis=0)])
 m = np.sum(k)
 global_B = A - 0.5*(1/m)*(k.T@k)
 
-fin_coms = []
+final_communities = []
+iter_tree_layers = {}
+iter_tree_communities= {}
+
 
 def split(G,node_list,depth = 0):
     B = global_B[np.ix_(node_list,node_list)]
@@ -29,29 +33,60 @@ def split(G,node_list,depth = 0):
     g1 = G.subgraph(n1)
     g2 = G.subgraph(n2)
 
+    if depth in iter_tree_layers:
+        iter_tree_layers[depth].append(node_list)
+    else:
+        iter_tree_layers[depth] = [node_list]
+
     if np.max(e)<=0:
-        fin_coms.append(node_list)
+        final_communities.append(node_list)
+        if depth in iter_tree_communities:
+            iter_tree_communities[depth].append(node_list)
+        else:
+            iter_tree_communities[depth] = [node_list]
         return
     if len(n1) == 0 :
-        fin_coms.append(node_list)
+        final_communities.append(node_list)
+        if depth in iter_tree_communities:
+            iter_tree_communities[depth].append(node_list)
+        else:
+            iter_tree_communities[depth] = [node_list]
         return
     if len(n2) == 0 :
-        fin_coms.append(node_list)
+        final_communities.append(node_list)
+        if depth in iter_tree_communities:
+            iter_tree_communities[depth].append(node_list)
+        else:
+            iter_tree_communities[depth] = [node_list]
         return
-
-    split(g1,n1)
-    split(g2,n2)
+    print(depth)
+    split(g1,n1,depth+1)
+    split(g2,n2,depth+1)
 
 split(G,global_nodelist)
-print(fin_coms)
+print(final_communities)
+
+
 def draw(sublist):
     n = len(sublist)
-    cmap = plt.get_cmap('viridis')
+    cmap = plt.get_cmap('tab20b')
     colors = [cmap(i) for i in np.linspace(0,1,n)]
-    #nx.draw_networkx_nodes(G,pos)
+    nx.draw_networkx_nodes(G,pos)
     for i in range(n):
         nx.draw_networkx_nodes(G,pos,nodelist = sublist[i], node_color = colors[i])
+        nx.draw_networkx_labels(G,pos,{a:i for a in sublist[i]} )
     nx.draw_networkx_edges(G,pos)
-draw(fin_coms)
 
+draw(final_communities)
 plt.show()
+for key,val in sorted(iter_tree_layers.items()):
+    newval = val
+    for key2,val2 in sorted(iter_tree_communities.items()):
+        if key>key2:
+            newval = newval + val2
+            print(newval)
+    val = newval
+for key,val in sorted(iter_tree_layers.items()):
+    draw(val)
+    plt.show()
+    print(key,":",val)
